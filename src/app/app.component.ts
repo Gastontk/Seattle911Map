e9857af9e9352ff1d180be7582da39324d0f0768
Gaston Kennedy <gastontk@hotmail.com>
2017-12-08 07:04:23 -0800

    Styling, setup address links for more info.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Http, Response,HttpModule } from '@angular/http';
 import 'rxjs';
 import 'rxjs/add/operator/map'
+// import { Ng}
 
 
 @Component({
@@ -17,8 +18,14 @@ export class AppComponent implements OnInit{
 	ind:string;
 	indexOfGreaterThan:string;
 	finalString:string
+  addressFix:any[] = [];
+  message = 'Welcome!'
+  statuses:any[] = [];
 //vars for google maps
 	positions:string[] = []
+  problem:string[] = []
+
+
 
 
   constructor(private http: Http){}
@@ -26,10 +33,7 @@ export class AppComponent implements OnInit{
   ngOnInit(){
   	this.get911Info()
   	var that = this
-  	setTimeout(
-  		function(){
-  			that.get911Info()
-  		},300000)
+
   	// var that = this;
   	// setInterval(
   	// 	function(){
@@ -39,59 +43,80 @@ export class AppComponent implements OnInit{
   }
 
   get911Info(){
-  	  	this.http.get('http://www2.seattle.gov/fire/realtime911/getRecsForDatePub.asp?action=Today&incDate=&rad1=des').subscribe(
+  	  	this.http.get('/test').subscribe(
   		(data:any):any => {
-  			// console.log('Data from server:', data._body); 
-  			this.htmlBody = data._body;
-  			var arr = this.htmlBody.toString().split(/<table/g)
+        // console.log('Data returned from server');
+  			 const htmlBody = data._body;
+        // console.log(htmlBody);
+  			var arr = htmlBody.split(/<table/g)
+        // console.log('arr[4] is', arr[4])
+  			var arrayHolder = arr[4];
+        // console.log(arrayHolder);
 
-  			this.htmlBody = arr[4];
-
-  			this.individualRecord = this.htmlBody.split(/valign="top">/g)
+  			this.individualRecord = arrayHolder.split(/">/)
   			
-
 
  //make sure to replace number with below length so as to use all recordes
 // this.individualRecord.length;
   			for( var x =0; x< this.individualRecord.length; x ++){
+          // console.log(x);
   				if(x%6 ==0){
-  					this.ind = this.individualRecord[x-1];
-  					this.indexOfGreaterThan = this.ind;
-   					this.addresses.push(this.indexOfGreaterThan);
+  					const ind = this.individualRecord[x];
+  					const indexOfGreaterThan = ind;
+   					this.addresses.push(indexOfGreaterThan);           
+            const responder =this.individualRecord[x+5]
+            const status = this.individualRecord[x+1];
+            const alive = this.individualRecord[x+2]
+            // console.log('status String is', status.substring(5,10))
+            const statusSubstring = status.substring(0, 22).split(/</g);
+
+            console.log('Alive is ', alive);
+            this.statuses.push(statusSubstring);
+
+
+            // const indexOf = this.responder.indexOf('<');
+            // console.log('Index of < is', indexOf)
+            this.problem.push(responder);
 
   				}
   			}
   			//remove first entry in array as it is blank
   			this.addresses.splice(0,1);
+        // console.log('this.addresses is:', this.addresses);
   			for( var x = 0; x< this.addresses.length; x++){
   				//split off < to clean up back of address.
   				// console.log(this.addresses[x].split(/</g));
 
-  				this.addresses[x] = (this.addresses[x].split(/</g)[0])
+  				this.addresses[x] = (this.addresses[x].split(/</g)[0]);
+          // console.log('results, different positions', this.addresses[x].split(/</g)[3])
+
+          
+
+     
 
 //get lat long from address
-
 			this.http.get('https://maps.googleapis.com/maps/api/geocode/json?address='+this.addresses[x]+', Seattle, WA').subscribe(
-							data =>
-								// console.log(data.json().results[0].geometry.location);
 //push lat long positions into positions array for array of markers
-								this.positions.push(data.json().results[0].geometry.location)
-							,
-							e => console.log('Their Was AN ERROR'),
-							()=> console.log('Subscription Finished')
-						 	
+							(data) =>{
+                  if(data.json().results[0].geometry != undefined){
+
+                     this.positions.push(data.json().results[0].geometry.location);
+
+                  }
+              }
 						)
-
-
-
-
-
-  				// this.positions.push(this.addresses[x] +', Seattle, Wa, USA');
   			}
-  			// console.log('Number of positions found:', this.positions.length)
 
   		}
   	)
+  }
+
+//handle link for more info
+  onclickAddress(i){
+    console.log(i, 'clicked');
+    const cut = this.problem[i];
+    const cutIndex = cut.indexOf('<');
+    this.message = 'Responders: ' + cut.substring(0,cutIndex) +  '\nEvent: ' + this.statuses[i][0] ;
   }
 
 //google maps stuff and converting address into lat long
